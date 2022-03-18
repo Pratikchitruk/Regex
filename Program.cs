f7e4b2ebb821387185d056e9cd6925618fb83194
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-18 12:35:07 +0530

    program.cs push for regex

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UserRegistrationProblem // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            EnterFirstName entername = new EnterFirstName();
+
+
+            Console.WriteLine("Enter the name");
+            string value = Console.ReadLine();
+               bool match=entername.ValidName(value);
+
+            if (match)
+                Console.WriteLine("name match");
+            else
+                Console.WriteLine("name not math");
+
+            EnterLastName.LastName();
+            Email.MyEmail();
+            MobileNo.MyNo();
+            Password.MyPassword();
+            AddUpeer.AddUpperPassword();
+            Addspacial.AddspacialPassword();
+
+
+                
+        }
+
+    }
+}
